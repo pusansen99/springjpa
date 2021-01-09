e6b8d08eeb2e00e3fdea2657797399499f196f38
Pusan Sen <pusansen99@gmail.com>
2021-01-10 00:26:53 +0530

    Pusan Sen

---
+++ src/main/java/com/deloitte/springjpa/topic/TopicService.java
@@ -0,0 +1,52 @@
+package com.deloitte.springjpa.topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
+            new Topic("1", "Spring",  "Spring Description"),
+            new Topic("2", "Java", "Java Description"),
+            new Topic("3", "Python", "Python Description")
+    ));
+
+    public List<Topic> getAllTopics() {
+        return topics;
+    }
+
+
+    public Topic getTopic(String id) {
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+
+    public void updateTopic(String id, Topic topic) {
+        for(int i =0; i<topics.size(); i++) {
+            Topic t = topics.get(i);
+            if(t.getId().equals(id)) {
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
+
+
+    public void deleteTopic(String id) {
+        for (int i = 0; i < topics.size(); i++) {
+            Topic t = topics.get(i);
+            if (t.getId().equals(id)) {
+                topics.remove(t);
+            }
+        }
+    }
+}
