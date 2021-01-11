3673acd3682888113b72db9a9794efbf5fca1842
Pusan Sen <pusansen99@gmail.com>
2021-01-11 23:01:48 +0530

    Pusan Sen

--- src/main/java/com/deloitte/springjpa/topic/TopicRepository.java
+++ src/main/java/com/deloitte/springjpa/topic/TopicRepository.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TopicRepository extends CrudRepository<Topic, String> {
-    //CRUDRepository Interface
+    //CRUDRepository
 }
